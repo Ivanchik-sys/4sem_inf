193f4c5d93aba93434a587d5f8e3705351d1dfed
Ivanchik-sys <78605362+Ivanchik-sys@users.noreply.github.com>
2022-04-30 23:26:11 +0300

    Create SpinLaunch.py
    Macroproject

---
+++ SpinLaunch.py
@@ -0,0 +1,58 @@
+from fenics import *
+from ufl import nabla_grad
+from ufl import nabla_div
+import os
+from math import *
+
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+mesh = Mesh(os.path.join(path, 'prototype_1.xml'))
+
+mesh_points = mesh.coordinates()
+
+nodes_z = [mesh_points[i][2] for i in range(len(mesh_points))]
+nodes_y = [mesh_points[i][1] for i in range(len(mesh_points))]
+nodes_x = [mesh_points[i][0] for i in range(len(mesh_points))]
+
+min_z, max_z = min(nodes_z), max(nodes_z)
+min_y, max_y = min(nodes_y), max(nodes_y)
+min_x, max_x = min(nodes_x), max(nodes_x)
+print('parameters:', min_x, max_x, min_y, max_y, min_z, max_z)
+print('lengths:', -min_x + max_x, -min_y + max_y, -min_z + max_z)
+
+nu = 0.34  # poisson coefficient
+E = 70 * 1e6  # module Unga
+Lambda = nu * E / ((1 + nu) * (1 - 2*nu))
+mu = E / (2 * (1 + nu))  # Lame coefficients aluminium
+
+omega = pi / 4
+theta = 0
+g = 9.8
+rho = 2700
+
+V = VectorFunctionSpace(mesh, 'P', 1)
+
+
+def epsilon(u):
+    return 0.5 * (nabla_grad(u) + nabla_grad(u).T)
+
+
+def sigma(u):
+    return Lambda * nabla_div(u) * Identity(d) + 2*mu*epsilon(u)
+
+
+f_g_ex, f_g_ey, f_g_ez = "0", "-g * sin(theta)", "-g * cos(theta)"
+#f_c_ex, f_c_ey, f_c_ez = 'abs(x[0]) * sqrt(x[0]*x[0] + x[1]*x[1]) * omega*omega', 'abs(x[1]) * sqrt(x[0]*x[0] + x[1]*x[1]) * omega*omega', '0'
+f_ex, f_ey, f_ez = f_g_ex, f_g_ey, f_g_ez
+
+f = Expression((f_ex, f_ey, f_ez), g=g, theta=theta)
+#f.omega = omega
+
+u = TrialFunction(V)
+d = u.geometric_dimension()
+v = TestFunction(V)
+a = inner(sigma(u), epsilon(v))*dx
+L = dot(f, v)*dx
+
+File('catapult/displacement.pvd') << u
